3fba0dd0c384f80b883fbcb6ca1b7879897ec016
Steve Geluso <stevegeluso@gmail.com>
2015-12-14 22:24:36 -0800

    fixing syntax errors

--- node-backend-starter-code/server.js
+++ node-backend-starter-code/server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var fs = require('fs');
 var path = require('path');
@@ -7,18 +9,19 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
-app.use('/', express.static(path.join(__dirname, 'public'));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.get('/favorites', function(req, res){
   var data = fs.readFileSync('./data.json');
   res.setHeader('Content-Type', 'application/json');
   res.send(data);
-;
+});
 
 app.get('favorites', function(req, res){
   if(!req.body.name || !req.body.oid){
     res.send("Error");
     return
+  }
   
   var data = JSON.parse(fs.readFileSync('./data.json'));
   data.push(req.body);
@@ -27,6 +30,6 @@ app.get('favorites', function(req, res){
   res.send(data);
 });
 
-app.list(3000, function(){
+app.listen(3000, function(){
   console.log("Listening on port 3000");
 });
