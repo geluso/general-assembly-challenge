90558ce23bc0fe6f0452d70d234e6c73a419a037
Steve Geluso <stevegeluso@gmail.com>
2015-12-16 00:57:25 -0800

    creating basic angular favorites page

--- public/js/controllers/favorites-controller.js
+++ public/js/controllers/favorites-controller.js
@@ -1,5 +1,13 @@
 var controllers = angular.module('searchApp');
 
-controllers.controller('FavoritesController', ['$scope', function($scope) {
+controllers.controller('FavoritesController', ['$scope', 'SearchService', function($scope, SearchService) {
   $scope.title = "FavoritesController";
+
+  $scope.favorites = [];
+
+  SearchService.favorites().then(function(response) {
+    $scope.favorites = response.data;
+  }, function(error) {
+    
+  });
 }]);
