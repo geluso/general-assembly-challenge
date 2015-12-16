a1f611a05be5b1f1f4091947b0fcd86f3944aefc
Steve Geluso <stevegeluso@gmail.com>
2015-12-16 00:09:46 -0800

    adding ng-route and creating templates for all pages

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,20 @@
-var app = angular.module('searchApp', []);
+var app = angular.module('searchApp', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+    when('/', {
+      templateUrl: 'js/templates/search.html',
+      controller: 'SearchController'
+    }).
+    when('/favorites.html', {
+      templateUrl: 'js/templates/favorites.html',
+      controller: 'FavoritesController'
+    }).
+    when('/details.html', {
+      templateUrl: 'js/templates/details.html',
+      controller: 'DetailController'
+    }).
+    otherwise({
+      redirectTo: '/'
+    });
+}]);
