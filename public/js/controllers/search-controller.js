31f9e873a3d342093537f68a5cf738df4273595e
Steve Geluso <stevegeluso@gmail.com>
2015-12-16 02:30:35 -0800

    implementing angular search error handling

--- public/js/controllers/search-controller.js
+++ public/js/controllers/search-controller.js
@@ -11,12 +11,20 @@ controllers.controller('SearchController', ['$scope', 'ApiService', function($sc
     // Default back to the placeholder if no terms are provided.
     terms = terms || $scope.placeholder;
 
+    // Optimistically reset error to false at the start of each search.
+    $scope.error = false;
+
     // Pass the terms to the API an have it initiate the search.
-    ApiService.search(terms).then(function(response) {
+    ApiService.search(terms).then(function(results) {
       // Hook up the results so the view can access them.
-      $scope.results = response.data.Search;
+      $scope.results = results;
     }, function(error) {
-      // Error
+      // There was an error. Place the error on the scope so it
+      // can be shown on the page.
+      $scope.error = error;
+
+      // Remove the previous results if an error occurred.
+      $scope.results = [];
     });
   };
 }]);
--- public/js/services/api.js
+++ public/js/services/api.js
@@ -1,5 +1,5 @@
 var services = angular.module('searchApp');
-services.factory('ApiService', ['$http', function($http) {
+services.factory('ApiService', ['$http', '$q', function($http, $q) {
   var FAVORITE_URL = "/favorites";
   var OMDB_URL = "https://www.omdbapi.com/";
 
@@ -7,7 +7,23 @@ services.factory('ApiService', ['$http', function($http) {
 
   api.search = function(terms) {
     var params = {s: terms};
-    return $http.get(OMDB_URL, {params: params});
+
+    var deferred = $q.defer();
+
+    $http.get(OMDB_URL, {params: params}).then(function(response) {
+      // Return the proper error message if there is a problem.
+      if (response.data.Response === "False") {
+        deferred.reject(response.data.Error);
+      // Otherwise, return the search results.
+      } else {
+        deferred.resolve(response.data.Search);
+      }
+    }, function(response) {
+      var errorText = 'HTTP ' + response.status + ' error. Try again.';
+      deferred.reject(errorText);
+    });
+
+    return deferred.promise;
   };
 
   api.addFavorite = function(name, oid) {
