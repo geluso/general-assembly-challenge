2eadb1b38a123c05322f1f9d03aa0b21a4341aa4
Steve Geluso <stevegeluso@gmail.com>
2015-12-15 21:05:43 -0800

    configuring app to user Heroku port

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var app = express();
 var fs = require('fs');
 var path = require('path');
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
@@ -30,6 +32,6 @@ app.post('/favorites', function(req, res){
   res.send(data);
 });
 
-app.listen(3000, function(){
-  console.log("Listening on port 3000");
+app.listen(app.get('port'), function(){
+  console.log("Listening on port ", app.get('port'));
 });
